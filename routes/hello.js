61081ef4d3a19c09f6fd57de36001498d7278531
Marshal Wu <marshal.wu@gmail.com>
2012-05-16 22:11:46 +0800

    演示如何抽象dao部分。

---
+++ routes/hello.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marshal
+ * Date: 12-5-15
+ * Time: 下午7:22
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.say = function (req, res) {
+    throw "say wrong";
+    res.send('hello');
+};
